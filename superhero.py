241351aebec4fa050114763118667a947c7af29f
alexmunoz143 <alexmunozcarcelen@gmail.com>
2026-01-14 09:32:55 +0100

    Add files via upload

---
+++ superhero.py
@@ -0,0 +1,13 @@
+
+name = "Aragon"       
+level = 1              
+money = 12.5          
+class_initial = 'W'    
+is_alive = True        
+
+print("--- HERO STATS ---")
+print("Name:", name)
+print("Level:", level)
+print("Gold:", money)
+print("Class:", class_initial)
+print("Alive:", is_alive)
